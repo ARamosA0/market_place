7f5e4d57e7aa977f8ac98f6366aa8ad910e6252e
ARamosA0 <lm20101a@gmail.com>
2022-06-13 21:01:01 -0500

    cochera metodos get,post, put

--- back/back_django/app/api/urls.py
+++ back/back_django/app/api/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.IndexView.as_view()),
-    path('cochera',views.CocheraView.as_view()),
-    path('cochera/<str:district_name>',views.CocheraByDistrict.as_view()),
+    path('cochera/',views.CocheraView.as_view()),
+    path('cochera/distrito/<str:district_name>/',views.CocheraByDistrict.as_view()),
+    path('cochera/<int:cochera_id>/',views.CocheraId.as_view()),
+    path('cochera/put/<int:cochera_id>/',views.CocheraViewChange.as_view())
 ]
--- back/back_django/app/api/views.py
+++ back/back_django/app/api/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -21,6 +22,7 @@ class IndexView(APIView):
         }
         return Response(context)
 
+
 class CocheraView(APIView):
     def get(self,request):
         dataCochera = Cochera.objects.all()
@@ -31,12 +33,44 @@ class CocheraView(APIView):
         }
         return Response(context)
 
+    def post(self,request, format=None):
+        serCochera = CocheraSerializer(data=request.data)
+        serCochera.is_valid(raise_exception=True)
+        serCochera.save()
+        return Response(serCochera.data)
+    
+
+class CocheraViewChange(APIView):
+    def put(self,request,cochera_id, format=None):
+        putCochera = Cochera.objects.get(pk=cochera_id)
+        print(putCochera)
+        serCochera = CocheraSerializer(putCochera, data=request.data)
+        if serCochera.is_valid():
+            serCochera.save()
+            return Response(serCochera.data)
+
+
+
 class CocheraByDistrict(APIView):
     def get(self,request, district_name):
-        dataCochera = Cochera.filter(district= district_name)
+        dataCochera = Cochera.objects.filter(district= district_name)
         serCochera = CocheraSerializer(dataCochera,many=True)
         context = {
             'status':True,
             'content':serCochera.data
         }
         return Response(context)
+
+
+class CocheraId(APIView):
+    def get(self,request,cochera_id):
+        dataCochera = Cochera.objects.get(pk=cochera_id)
+        serCochera = CocheraSerializer(dataCochera)
+        context = {
+            'status':True,
+            'content':serCochera.data,
+            'message':'data'
+        }
+        print(context)
+        return Response(context)
+
